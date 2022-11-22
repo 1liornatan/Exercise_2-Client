1124cc7a83b9792622aa5657353c20b5f830b570
1liornatan <1liornatan@gmail.com>
2022-11-23 00:10:52 +0200

    Version 2

--- src/client/SimpleMain.java
+++ src/client/SimpleMain.java
@@ -2,8 +2,12 @@ package client;
 
 public class SimpleMain {
 
+    private static final String HOST = "localhost";
+    private static final String PORT = "8989";
+    private static final String PROTOCOL = "http";
+
     public static void main(String[] args) {
-        Requests req = new Requests("localhost:8989");
+        Requests req = new Requests(PROTOCOL + "://" + HOST + ":" + PORT);
         req.start();
     }
 }
